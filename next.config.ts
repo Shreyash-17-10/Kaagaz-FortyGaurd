28f381ae832a86814b02d11d67b7efa7b6674252
Shreyash-17-10 <tshreyash00@gmail.com>
2026-08-31 16:35:21 +0530

    Move Next.js to root directory for Vercel Zero-Config deployment

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,10 @@ const nextConfig: NextConfig = {
     return [
       {
         source: "/api/:path*",
-        destination: "/api/:path*",
+        destination:
+          process.env.NODE_ENV === "development"
+            ? "http://127.0.0.1:8000/api/:path*"
+            : "/api/index.py",
       },
     ];
   },
